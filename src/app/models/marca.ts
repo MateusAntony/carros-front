62aa89faa0f40337870d40ff5e3ca24e37fb38a8
MateusAntony <mateus.medeiros110@gmail.com>
2025-07-29 16:49:40 -0300

    marca

--- src/app/models/marca.ts
+++ src/app/models/marca.ts
@@ -2,5 +2,11 @@ export class Marca {
 
     id!: number;
     nome!: string;
+    cnpj!: string;
 
+    constructor( id: number, nome: string,cnpj: string){
+        this.id = id;
+        this.nome = nome;
+        this.cnpj = cnpj;
+    }
 }
